3d27c74d753d4fd669ca44b9ed76dc58f49882bb
Igor Shelginskiy <engwar@macbook-pro-igor.local>
2018-10-21 10:02:49 +0300

    create two objects( programming and AR)

--- AR first task/ViewController.swift
+++ AR first task/ViewController.swift
@@ -17,17 +17,51 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Show world origin
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
+        
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        //Load Campus
+        loadCampus()
+        
+        loadMainBuilding()
+       
+    }
+    
+    func loadCampus(){
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/campus.scn")!
         
         // Set the scene to the view
         sceneView.scene = scene
+    }
+    
+    
+    func loadMainBuilding() {
+        let boxNode = SCNNode()
+        let box = SCNBox(width: 3, height: 1, length: 1, chamferRadius: 0)
+        box.firstMaterial?.diffuse.contents = #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1)
+        boxNode.geometry = box
+        boxNode.position.y += 0.001
+        
+        let planeNode = SCNNode()
+        let plane = SCNPlane(width: 3.5, height: 1.5)
+        plane.firstMaterial?.diffuse.contents = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        planeNode.geometry = plane
+        planeNode.eulerAngles.x += -.pi/2
+        planeNode.position.y -= 0.5
+        
+        let campusNode = SCNNode()
+        campusNode.position = SCNVector3(0, -1, -3)
+        campusNode.scale = SCNVector3(0.5, 0.5, 0.5)
+        campusNode.addChildNode(boxNode)
+        campusNode.addChildNode(planeNode)
+        sceneView.scene.rootNode.addChildNode(campusNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,7 +71,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let configuration = ARWorldTrackingConfiguration()
 
         // Run the view's session
-        sceneView.session.run(configuration)
+        sceneView.session.run(configuration) 
     }
     
     override func viewWillDisappear(_ animated: Bool) {

2555d34796235ccbe8bfefcec46aa1da00503696
Igor Shelginskiy <engwar@macbook-pro-igor.local>
2018-10-22 16:44:01 +0300

    Added programming multiply objects

--- AR first task/ViewController.swift
+++ AR first task/ViewController.swift
@@ -50,11 +50,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         boxNode.position.y += 0.001
         
         let planeNode = SCNNode()
-        let plane = SCNPlane(width: 3.5, height: 1.5)
+        let plane = SCNPlane(width: 5, height: 2)
         plane.firstMaterial?.diffuse.contents = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         planeNode.geometry = plane
         planeNode.eulerAngles.x += -.pi/2
         planeNode.position.y -= 0.5
+        
+        for y in stride(from: Float(-5.0), to: 5.0, by: 0.5) {
+            let treeNode = loadTree()
+            treeNode.position.x += 2
+            treeNode.position.y = y
+            treeNode.position.z += 0.25
+            planeNode.addChildNode(treeNode)
+        }
         
         let campusNode = SCNNode()
         campusNode.position = SCNVector3(0, -1, -3)
@@ -62,6 +70,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         campusNode.addChildNode(boxNode)
         campusNode.addChildNode(planeNode)
         sceneView.scene.rootNode.addChildNode(campusNode)
+    }
+    
+    func loadTree() -> SCNNode {
+        let treeNode = SCNNode()
+        let cylinder = SCNCylinder(radius: 0.05, height: 0.5)
+        cylinder.firstMaterial?.diffuse.contents = #colorLiteral(red: 0.3176470697, green: 0.07450980693, blue: 0.02745098062, alpha: 1)
+        treeNode.geometry = cylinder
+        treeNode.eulerAngles.x -= .pi/2
+        
+        
+        let sphereNode = SCNNode()
+        let sphere = SCNSphere(radius: 0.2)
+        sphere.firstMaterial?.diffuse.contents = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        sphereNode.geometry = sphere
+        sphereNode.position.y -= 0.4
+        treeNode.addChildNode(sphereNode)
+        
+        return treeNode
     }
     
     override func viewWillAppear(_ animated: Bool) {
